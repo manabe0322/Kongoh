71bb2c784c44e547b8d3bcaa249fcc8faa386000
Sho_Manabe <manabe@fp.med.kyoto-u.ac.jp>
2022-10-21 17:12:38 +0900

    Delete secret

--- secret/private_program/rand_gt.R
+++
@@ -1,4 +0,0 @@
-# Make random genotypes of unrelated individuals based on population data
-makeRandGt <- function(np, afList){
-  mapply(sample, sapply(sapply(afList, names), as.numeric), 2 * np, prob = afList, replace = TRUE)
-}
